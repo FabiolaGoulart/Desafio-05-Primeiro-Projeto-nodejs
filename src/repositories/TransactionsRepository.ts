d1b12f3ea806ed839bb0b217b9597ecbc70c84d6
Fabíola Goulart <fabiola.goulart.si@gmail.com>
2020-05-20 11:09:45 -0300

    Complementando desafio 5 - GoStack

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,48 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'income') {
+      this.balance.income += value;
+    }
+
+    if (type === 'outcome') {
+      this.balance.outcome += value;
+    }
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
